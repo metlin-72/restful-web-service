f7f8539438f1591d77f6140da8a0ba74edc971cc
이노가드_김원기 <metlin@inogard.co.kr>
2020-07-14 17:55:21 +0900

    2020.07.14 회사변경 2

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -14,4 +15,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}fjqbgurqls1!")
                 .roles("USER");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+    }
+
+
 }
